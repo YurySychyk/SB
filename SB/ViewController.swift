1a90cc430c2f0803ef206bc018348a68e2adb211
Yury Sychyk <kauz@tut.by>
2022-02-10 09:01:21 +0300

    Initial Commit

---
+++ SB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SB
+//
+//  Created by Yury Sychyk on 10.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
